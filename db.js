59b93314fb7b2544c59f2ef45c0b2bad2364a96b
snmng <kjj17189@naver.com>
2026-01-21 17:31:42 +0900

    change pool process

--- db.js
+++ db.js
@@ -1,13 +1,6 @@
 // db.js (CommonJS)
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "tmdals",
-  database: "blog_db",
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+const pool = mysql.createPool(process.env.MYSQL_URL);
 
 module.exports = pool;
