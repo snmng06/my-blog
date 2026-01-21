f0a10226cef0e702886f9e0d6fd172c66d047263
snmng <kjj17189@naver.com>
2026-01-21 17:35:29 +0900

    db.js modify

--- db.js
+++ db.js
@@ -1,5 +1,10 @@
 // db.js (CommonJS)
-const mysql = require("mysql2/promise");
+const mysql = require('mysql2/promise');
+
+if (!process.env.MYSQL_URL) {
+  console.error("MYSQL_URL is missing");
+  process.exit(1);
+}
 
 const pool = mysql.createPool(process.env.MYSQL_URL);
 
